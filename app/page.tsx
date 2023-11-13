aad9f39da555ef1a2b43d43891c39226cf77021b
braincee <kwesi938@gmail.com>
2023-11-13 13:51:20 +0000

    Add Discord component and Dark and light mood

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import SlackMessage from '@/components/SlackMessage'
 import TelegramMessage from '@/components/TelegramMessage'
 import { Box } from '@mui/joy'
+import DiscordMessage from '@/components/DiscordMessage'
+import ModeToggle from '@/components/ModeToggle'
 
 export default function Home() {
   return (
     <main>
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
+        <ModeToggle />
         <SlackMessage />
         <TelegramMessage />
+        <DiscordMessage />
       </Box>
     </main>
   )
