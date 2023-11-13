a47b98f96a9e5318f94387d93dcfde63d921234f
Achilles-Dev <solohag92@gmail.com>
2023-11-13 09:20:31 +0000

    Add telegram message component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import SlackMessage from '@/components/SlackMessage'
+import TelegramMessage from '@/components/TelegramMessage'
 import { Box } from '@mui/joy'
 
 export default function Home() {
   return (
     <main>
-      <Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
         <SlackMessage />
+        <TelegramMessage />
       </Box>
     </main>
   )
