c0c751b7970b47f0a04705f8c5bce92432f8eab1
braincee <kwesi938@gmail.com>
2023-11-14 11:05:37 +0000

    light and dark mode

--- components/Discord.tsx
+++ components/Discord.tsx
@@ -1,8 +1,11 @@
-import { Avatar, Box, Button, Sheet, Stack, Typography } from '@mui/joy'
-import Image from 'next/image'
-import Link from 'next/link'
+'use client'
+
+import { Avatar, Box, Sheet, Stack, Typography, List, ListItem, useTheme } from '@mui/joy';
 
 const Discord = () => {
+    const theme = useTheme();
+    const textColor = theme.palette.mode === 'dark' ? '#FFFFFF' : '#000000';
+
   return (
     <Box sx={{ backgroundColor: 'pink', py: '20px' }}>
       <Sheet>
@@ -11,24 +14,26 @@ const Discord = () => {
             padding: '8px 20px',
             display: 'flex',
             flexDirection: 'row',
+            gap: '10px',
           }}
         >
           <Avatar />
           <Stack
             sx={{
               flex: '1 1 0',
-              margin: '-12px -8px -16px -16px',
+              margin: '-8px -10px -16px -6px',
               minWidth: 0,
               padding: '8px 8px 8px 16px',
-              fontFamily: 'Lato san-serif',
+              fontFamily: 'Lato, sans-serif',
             }}
           >
-            <Stack
+            <List
               sx={{
                 display: 'flex',
                 flexDirection: 'row',
-                gap: '4px',
+                gap: '10px',
                 alignItems: 'center',
+                paddingBottom: '2px',
               }}
             >
               <Typography
@@ -44,21 +49,11 @@ const Discord = () => {
                   minHeight: 0,
                 }}
               >
-                Typescript Community #Updates
+                Typescript Community
               </Typography>
 
-              <span className='text-[12px]'>09/24/2022 6:45 PM</span>
-            </Stack>
-            <Stack
-              sx={{
-                marginBottom: '4px',
-                width: '100%',
-              }}
-            >
-              <Link href='https://devblogs.microsoft.com/typescript/announcing-typescript-5-2/'>
-                https://devblogs.microsoft.com/typescript/announcing-typescript-5-2/
-              </Link>
-            </Stack>
+              <Typography sx={{ fontSize: '12px' }}>Today at 6:45 PM</Typography>
+            </List>
             <Stack
               sx={{
                 width: '100%',
@@ -66,12 +61,12 @@ const Discord = () => {
                 display: 'flex',
                 flexDirection: 'row',
                 alignItems: 'stretch',
-                margin: '0 0 8px',
                 position: 'relative',
+                background: '#dedede'
               }}
             >
-              <span className='top-0 bottom-0 left-[-20px] absolute w-[20px]'></span>
-              <span className='rounded-lg w-[4px] shrink-0 bg-[#dddddd]'></span>
+              <Typography sx={{ top: 0, bottom: 0, left: '-20px', position: 'absolute', width: '20px' }}></Typography>
+              <Typography sx={{ borderRadius: '4px', width: '4px', flexShrink: 0, backgroundColor: '#00d2ff' }}></Typography>
               <Stack
                 sx={{
                   flex: '1 1 0',
@@ -80,46 +75,26 @@ const Discord = () => {
                   width: '100%',
                 }}
               >
-                <div className='gap-2 text-[15px] leading-[1.46668]'>
-                  <span className='flex items-center'>
-                    <span className='font-black mb-4'>Typescript</span>
-                  </span>
-                  <span>Daniel Rossenwasser</span>
-                </div>
-                <span className='font-bold text-[15px] leading-[1.46668] mt-4'>
-                  <Link href='#'>Announcing Typescript 5.2 - Typescript</Link>
-                </span>
-                <span className='mt-4 md:max-w-[65%]'>
-                  <p>
-                    Today we’re excited to announce the release of TypeScript
-                    5.2! If you’re not familiar with TypeScript, it’s a language
-                    that builds on top of JavaScript by making it possible to
-                    declare and describe types. Writing types in our code allows
-                    us to explain intent and have other tools check our code to
-                    catch mistakes like typos,
-                  </p>
-                </span>
-                <Stack
-                  sx={{
-                    width: '360px',
-                    position: 'relative',
-                    maxWidth: '100%',
-                    marginTop: '5px',
-                  }}
-                >
-                  <div className='pt-[75%] relative'>
-                    <div className='h-[100%] left-0 top-0 absolute'>
-                      {/* <Image src={MessageImage} alt='Message Video' /> */}
-                    </div>
-                  </div>
-                </Stack>
+                <List>
+                    <ListItem>
+                    <Typography sx={{ fontWeight: 'bold', marginBottom: '4px' }}>Daniel Rossenwasser</Typography>
+                    </ListItem>
+                </List>
+                <Typography sx={{ fontWeight: 'bold', fontSize: '15px', lineHeight: '1.46668', marginTop: '4px' }}>
+                  Announcing Typescript 5.2 - Typescript
+                </Typography>
+                <Typography sx={{ marginTop: '4px', maxWidth: '65%', color: textColor }}>
+                    Today we’re excited to announce the release of TypeScript 5.2! If you’re not familiar with TypeScript, it’s a language
+                    that builds on top of JavaScript by making it possible to declare and describe types. Writing types in our code allows
+                    us to explain intent and have other tools check our code to catch mistakes like typos,
+                </Typography>
               </Stack>
             </Stack>
           </Stack>
         </Stack>
       </Sheet>
     </Box>
-  )
-}
+  );
+};
 
-export default Discord
+export default Discord;
